924f28eae9ca9c8a80b5934df3e08d259afb7e99
Alberto Oliva <86741575+yourbeautyweb@users.noreply.github.com>
2021-07-02 16:52:28 -0300

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,40 @@
+
+/*--------------*/
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './css/style.css',
+        './js/main.js'
+        '/images/little.jpg',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('./images/little.jpg');
+      });
+    }
+  }));
+});
