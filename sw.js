18959cee6bebf55fdbc9020c3d82318f5fb1948b
Alberto Oliva <86741575+yourbeautyweb@users.noreply.github.com>
2021-07-06 17:00:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,3 +24,19 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
