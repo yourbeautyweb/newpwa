9def1390f379b60a8b08937d85edb84e7a1ee620
Alberto Oliva <86741575+yourbeautyweb@users.noreply.github.com>
2021-07-27 18:30:15 -0300

    Add files via upload

---
+++ agencia/nosotros.php
@@ -0,0 +1,11 @@
+
+<div class="card">
+<h2><span style="font-size:40px; color:#d50df7;">U</span>tilizar Remise.ar</h2>
+<p class="p-suave">Reg&iacute;strate y obten tu c&oacute;digo para poder hacer uso de las diferentes agencias en red.</p>
+<a href="http://localhost/ajax/r/index.php" class="BUTTON_OM">lo quiero</a>
+</div>
+<div class="card">
+<h2><span style="font-size:40px; color:#d50df7;">Q</span>ue es Remise.ar</h2>
+<p class="p-suave">Es una red de agencias de remis, registra tu agencia para sumar clientes.</p>
+<a href="http://localhost/ajax/interno/formulario_alta.php" class="BUTTON_OM">vea como</a>
+</div>
